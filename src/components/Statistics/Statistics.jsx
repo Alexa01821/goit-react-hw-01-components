86a4282b6a8004e9788a19cfb43be2d55f39ef74
Alexa01821 <sahunia2004@gmail.com>
2023-09-06 12:28:25 +0300

    fix-commit

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { StatisticsContainer } from './StatisticsStyled';
-import {getRandomHexColor} from './getRandomHexColor'
-
+import { getRandomHexColor } from './getRandomHexColor';
 
 export const Statistics = ({ title, stats }) => {
   return (
     <StatisticsContainer>
-      <h2 className="title">{title}</h2>
+      {title && (<h2 className="title">{title}</h2>)}
 
       <ul className="stat-list">
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li key={id} className="stat-item" style={{backgroundColor: getRandomHexColor()}}>
+            <li
+              key={id}
+              className="stat-item"
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
               <span className="label">{label}</span>
               <span className="percentage">{percentage}%</span>
             </li>
